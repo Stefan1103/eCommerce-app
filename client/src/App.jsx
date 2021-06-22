ca0c0b065e7df273983397cc33040ccd8e5519ac
Stefan1103 <stefan.stevanovski11@gmail.com>
2021-06-22 22:17:51 +0200

    added styles

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,10 @@
 import './Sass/app.scss';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
 	return (
 		<div className="app">
+			<Navbar />
 			<div className="sections"> HELLO HELLO WORLEDS!</div>
 		</div>
 	);
--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 const Navbar = () => {
 	return (
 		<div className="customNavbar">
 			<div className="navWrapper">
 				<div className="left">
-					<h2 className="Logo">Flea Market</h2>
+					<h2 className="logo">
+						<FontAwesomeIcon icon="fa-solid fa-cloud-meatball" />Flea Market
+					</h2>
 				</div>
 				<div className="middle">
 					<form>
-						<input type="text" placeholder="search for items" />
-						<button type="submit" />
+						<input type="text" placeholder="search using name or category ex: Apple or 'meat'" />
+						<button className="btn-search" type="submit">
+							Search
+						</button>
 					</form>
 				</div>
 				<div className="right">
