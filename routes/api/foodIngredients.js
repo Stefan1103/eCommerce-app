fb1dc98639b2035143386c7941c53bb8fd82cdb7
Stefan1103 <stefan.stevanovski11@gmail.com>
2021-06-22 13:36:32 +0200

    commented

--- routes/api/foodIngredients.js
+++ routes/api/foodIngredients.js
@@ -19,7 +19,7 @@ const FoodIngredients = require('../../models/FoodIngredients');
 
 router.get('/', async (req, res) => {
 	try {
-		const ingredients = await FoodIngredients.find();
+		const ingredients = await FoodIngredients.find().limit(10);
 		res.json(ingredients);
 	} catch (error) {
 		console.error(error.message);
@@ -59,20 +59,22 @@ router.get('/:ingredient_cat', async (req, res) => {
 	}
 });
 
-// @route GET api/foodIngredients
-// desc GET ALL FOOD INGREDIENTS
+// admin to add new food ingredients to the db (if we make users model)
+///////////////////////////////////////////////////
+// @route POST api/foodIngredients
+// desc POST POST new ingredients
 // @access Public
 
-router.post('/new', async (req, res) => {
-	try {
-		const newIngredients = FoodIngredients(req.body);
+// router.post('/new', async (req, res) => {
+// 	try {
+// 		const newIngredients = FoodIngredients(req.body);
 
-		const savedIngredient = await newIngredients.save();
-		res.json(savedIngredient);
-	} catch (error) {
-		console.error(error.message);
-		res.status(500).send('Server Error');
-	}
-});
+// 		const savedIngredient = await newIngredients.save();
+// 		res.json(savedIngredient);
+// 	} catch (error) {
+// 		console.error(error.message);
+// 		res.status(500).send('Server Error');
+// 	}
+// });
 
 module.exports = router;
