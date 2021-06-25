8941229d77072617ea1be13d86a52e7a8bc63124
Stefan1103 <stefan.stevanovski11@gmail.com>
2021-06-25 13:48:03 +0200

    added file structure for redux and added empty store

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,18 @@
 import './Sass/app.scss';
 import Navbar from './components/Navbar/Navbar';
 
+//Redux
+import { Provider } from 'react-redux';
+import store from './redux/store';
+
 function App() {
 	return (
-		<div className="app">
-			<Navbar />
-			<div className="sections"> HELLO HELLO WORLEDS!</div>
-		</div>
+		<Provider store={store}>
+			<div className="app">
+				<Navbar />
+				<div className="sections"> HELLO HELLO WORLEDS!</div>
+			</div>
+		</Provider>
 	);
 }
 
