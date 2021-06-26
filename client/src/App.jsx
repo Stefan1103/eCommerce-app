0a1879b190d13def4fe4fa4387bf0b6078343622
Stefan1103 <stefan.stevanovski11@gmail.com>
2021-06-26 14:18:25 +0200

    added alert component and actions and reducer for alert in redux

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,21 @@
 import './Sass/app.scss';
-import Navbar from './components/Navbar/Navbar';
+import Navbar from './components/layout/Navbar';
 import Main from './components/Main/Main';
+import CustomAlert from './components/layout/CustomAlert';
 
 //Redux
 import { Provider } from 'react-redux';
 import store from './redux/store';
+console.log(store);
 
 function App() {
 	return (
 		<Provider store={store}>
 			<div className="app">
 				<Navbar />
+
 				<div className="sections">
-					{' '}
+					<CustomAlert />
 					<Main />
 				</div>
 			</div>
