6002c290f008df004cc3a6ddc3dc32c612d90301
Stefan1103 <stefan.stevanovski11@gmail.com>
2021-07-08 15:43:17 +0200

    added scss files and basic markup for cart page

--- client/src/components/Cart/Cart.jsx
+++ client/src/components/Cart/Cart.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import shopingCart from '../../assets/shopping-cart.png';
 
 const Cart = () => {
-	return <div className="container">CART PAGE</div>;
+	console.log('SLIKA U KART', shopingCart);
+	return (
+		<div className="container">
+			<div className="wrapper-cart row">
+				<div className="img-container-cart col-4">
+					<img className="img-fluid" src={shopingCart} />
+				</div>
+				<div className="cart-items col-8">
+					<h1>Your Cart</h1>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Cart;
