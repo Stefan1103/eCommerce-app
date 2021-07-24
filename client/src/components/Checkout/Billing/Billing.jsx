ba17cf778bf58bc798a37586cdfad913fcd72628
Stefan1103 <stefan.stevanovski11@gmail.com>
2021-07-24 16:51:05 +0200

    started the billing page markup and styles.

--- client/src/components/Checkout/Billing/Billing.jsx
+++ client/src/components/Checkout/Billing/Billing.jsx
@@ -1,7 +1,40 @@
 import React from 'react';
 
 const Billing = () => {
-	return <div>BILLING PAGE</div>;
+	return (
+		<div className="container container-billing">
+			<div className="billing-wrapper bg-light">
+				<h1>PAYMENT</h1>
+				<div className="billing-wrapper-inner">
+					<form className=" row col-12" id="payment">
+						<div className="left col-6">
+							<label for="creditCardHolder">Credit Card Holder :</label>
+							<label for="creditCards">Choose a car:</label>
+							<label for="creditCardNumber">Credit Card Number :</label>
+							<label for="code">CVV code :</label>
+							<label for="expireDate">Expire Date :</label>
+						</div>
+
+						<div className="right col-6">
+							<input name="creditCardHolder" type="text" placeholder="James Bond" />
+
+							<select name="creditCards" id="creditCards">
+								<option value="masterCard">Master card</option>
+								<option value="visaCard">Visa Card</option>
+							</select>
+
+							<input name="creditCardNumber" type="number" placeholder="________________" />
+
+							<input name="code" type="number" placeholder="ex. 235" />
+
+							<input name="expireDate" type="month" placeholder="MM/YY" />
+						</div>
+					</form>
+				</div>
+				<button className="btn-add-cart">Pay</button>
+			</div>
+		</div>
+	);
 };
 
 export default Billing;
