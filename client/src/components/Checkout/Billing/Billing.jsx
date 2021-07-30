b7811dfae454c3a997dfbe31286b5273abf6f55b
Stefan1103 <stefan.stevanovski11@gmail.com>
2021-07-30 18:16:16 +0200

    added responsiveness to the billing page.

--- client/src/components/Checkout/Billing/Billing.jsx
+++ client/src/components/Checkout/Billing/Billing.jsx
@@ -18,29 +18,30 @@ const Billing = () => {
 				</div>
 
 				<div className="billing-wrapper-inner">
-					<form className=" row col-12" id="payment" onSubmit={paymentHandle}>
-						<div className="left col-6">
-							<label for="creditCardHolder">Credit Card Holder :</label>
-							<label for="creditCards">Choose a car:</label>
-							<label for="creditCardNumber">Credit Card Number :</label>
-							<label for="code">CVV code :</label>
-							<label for="expireDate">Expire Date :</label>
-						</div>
-
-						<div className="right col-6">
-							<input name="creditCardHolder" type="text" placeholder="James Bond" />
-
-							<select name="creditCards" id="creditCards">
-								<option value="masterCard">Master card</option>
-								<option value="visaCard">Visa Card</option>
-							</select>
-
-							<input name="creditCardNumber" type="number" placeholder="________________" />
-
-							<input name="code" type="number" placeholder="ex. 235" />
-
-							<input name="expireDate" type="month" placeholder="MM/YY" />
-						</div>
+					<form className="row col-12" id="payment" onSubmit={paymentHandle}>
+						<label className="col-12 col-md-6" for="creditCardHolder">
+							Credit Card Holder :
+						</label>
+						<input className="col-12 col-md-6" name="creditCardHolder" type="text" placeholder="James Bond" />
+						<label className="col-12 col-md-6" for="creditCards">
+							Choose a car:
+						</label>
+						<select className="col-12 col-md-6" name="creditCards" id="creditCards">
+							<option value="masterCard">Master card</option>
+							<option value="visaCard">Visa Card</option>
+						</select>
+						<label className="col-12 col-md-6" for="creditCardNumber">
+							Credit Card Number :
+						</label>
+						<input className="col-12 col-md-6" name="creditCardNumber" type="number" placeholder="________________" />
+						<label className="col-12 col-md-6" for="code">
+							CVV code :
+						</label>
+						<input className="col-12 col-md-6" name="code" type="number" placeholder="ex. 235" />
+						<label className="col-12 col-md-6" for="expireDate">
+							Expire Date :
+						</label>
+						<input className="col-12 col-md-6" name="expireDate" type="month" placeholder="MM/YY" />
 					</form>
 				</div>
 				<button type="submit" className="btn-add-cart" form="payment">
