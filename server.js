06efce677781c56a5b2f7300f14e777b14bd752c
Stefan1103 <stefan.stevanovski11@gmail.com>
2021-09-10 16:49:40 +0200

    cors commit

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const connectDB = require('./config/db');
 require('dotenv').config();
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors());
 
 // Connect Database
 connectDB();
