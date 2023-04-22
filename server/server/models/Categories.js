fea7345deafc641f6527a1647d22feb45a42e79a
Stefan1103 <stefan.stevanovski11@gmail.com>
2023-04-22 21:33:14 +0200

    cleanup on doubled files.

--- server/server/models/Categories.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-const CategoriesSchema = new mongoose.Schema({
-	category: { type: String, unique: true },
-});
-
-module.exports = Categories = mongoose.model('Categories', CategoriesSchema);
